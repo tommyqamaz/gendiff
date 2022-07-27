825a3d21d17e087b76b4b67662c638df292e64a0
qamaz <solgrini@yandex.ru>
2022-07-27 16:29:37 +0300

    add description to parser

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,9 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+    )
     parser.add_argument("-f", "--format", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
