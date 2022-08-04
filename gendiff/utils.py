d5477cce65342e10fd9c322348e68fb41067f894
tommyqamaz <solgrini@yandex.ru>
2022-08-04 18:37:35 +0300

    minor style refactoring

--- gendiff/utils.py
+++ gendiff/utils.py
@@ -3,6 +3,8 @@ import yaml
 from yaml import Loader
 from itertools import chain
 
+EMPTY = "89e604e4d0ab221519c1e9d97fac6e1819f5dfbc6ef4d0fade2e62ccd68780bf"
+
 
 def get_file(path: str) -> dict:
     """Read the file from path and returns dictionary."""
@@ -27,32 +29,32 @@ def _translate(value):
     return value
 
 
-def _update_diff_nested(diff, key, output1, output2, empty):
+def _update_diff_nested(diff, key, output1, output2):
     mode = "nested"
     if isinstance(output1, dict) and isinstance(output2, dict):
         output1 = get_diff_as_dict(output1, output2, mode)
         diff.update({key: output1})
     elif output1 == output2:
         diff.update({key: output1})
-    elif output1 == empty:
+    elif output1 == EMPTY:
         diff.update({f"+{key}": output2})
-    elif output2 == empty:
+    elif output2 == EMPTY:
         diff.update({f"-{key}": output1})
     else:
         diff.update({f"-{key}": output1})
         diff.update({f"+{key}": output2})
 
 
-def _update_diff_plain(diff, key, output1, output2, empty):
+def _update_diff_plain(diff, key, output1, output2):
     mode = "plain"
     if isinstance(output1, dict) and isinstance(output2, dict):
         output1 = get_diff_as_dict(output1, output2, mode)
         diff.update({key: output1})
     elif output1 == output2:
         diff.update({key: {"_no_changes": output1}})
-    elif output1 == empty:
+    elif output1 == EMPTY:
         diff.update({key: {"_added": output2}})
-    elif output2 == empty:
+    elif output2 == EMPTY:
         diff.update({key: {"_removed": output1}})
     else:
         diff.update({key: {"_added": output2, "_removed": output1}})
@@ -60,18 +62,17 @@ def _update_diff_plain(diff, key, output1, output2, empty):
 
 def get_diff_as_dict(file1, file2, mode="nested"):
     """Returns the difference between two tree-like objects as a dictionary."""
-    empty = "89e604e4d0ab221519c1e9d97fac6e1819f5dfbc6ef4d0fade2e62ccd68780bf"
     keys1 = file1.keys()
     keys2 = file2.keys()
     all_keys = get_all_keys(keys1, keys2)
 
     diff = {}
     for key in all_keys:
-        output1 = _translate(file1.get(key, empty))
-        output2 = _translate(file2.get(key, empty))
+        output1 = _translate(file1.get(key, EMPTY))
+        output2 = _translate(file2.get(key, EMPTY))
         if mode == "nested":
-            _update_diff_nested(diff, key, output1, output2, empty)
+            _update_diff_nested(diff, key, output1, output2)
         else:
-            _update_diff_plain(diff, key, output1, output2, empty)
+            _update_diff_plain(diff, key, output1, output2)
 
     return diff
